8be96799824adb3aa6d22bf1196bac3df79a1a8b
KeerthanaCG <keerthanagirishchn@gmail.com>
2023-10-13 18:54:16 +0530

    Completed website

---
+++ script/script.js
@@ -0,0 +1,109 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+let menu =document.querySelector('#menu-bar');
+let navbar =document.querySelector('.navbar');
+let videoBtn =document.querySelectorAll('.vid-btn');
+
+
+
+
+
+
+window.onscroll = () =>{
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    
+}
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+    
+});
+
+
+searchBtn.addEventListener('click', () =>{
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+    
+});
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
+});
+let themeButton = document.querySelector('#theme-btn');
+themeButton.onclick = () => {
+    themeButton.classList.toggle('fa-moon');
+    themeButton.classList.toggle('fa-sun');
+  
+    if (themeButton.classList.contains('fa-moon')) {
+      document.body.classList.add('active');
+    } else {
+      document.body.classList.remove('active');
+    }
+  };
+  
+
+
+
+videoBtn.forEach(btn =>{
+   btn.addEventListener('click', ()=>{
+    document.querySelector('.controls .active').classList.remove('active');
+    btn.classList.add('active');
+    let src=btn.getAttribute('data-src');
+    document.querySelector('#video-slider').src =src;
+   });
+});
+
+var swiper = new Swiper(".reviews-slider", {
+
+    spaceBetween: 20,
+    loop:true,
+    autoplay:{
+        delay:3500,
+        disbleOnInteraction:false,
+    },
+    breakpoints:{
+        640:{
+            slidesPerView: 1,
+        },
+        760:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+    }
+
+});
+
+var swiper=new Swiper(".collaborations-slider",{
+    spaceBetween: 50,
+    loop:true,
+    autoplay:{
+        delay:2500,
+        disbleOnInteraction:false,
+    },
+    breakpoints:{
+        640:{
+            slidesPerView: 1,
+        },
+        760:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+    }
+
+});
